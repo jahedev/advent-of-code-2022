d52a28723ee9d63e73ada9f92acb6f11b00843ea
M. Jahed Hossain <jahedev@gmail.com>
2022-12-01 18:41:35 -0500

    start part 2

--- day-01/day01.py
+++ day-01/day01.py
@@ -7,7 +7,7 @@ is separated by a single empty line.
 """
 
 
-def day_01(in_filename):
+def day_01_part_1(in_filename):
     # amount carried by the current richest elf
     max_calories = -maxsize
 
@@ -29,6 +29,32 @@ def day_01(in_filename):
         return max(max_calories, curr_calories)
 
 
+def day_01_part_2(in_filename):
+    # the elf who carries the most calories
+    richest_elf = -1
+    # amount carried by the current richest elf
+    max_calories = -maxsize
+
+    with open(in_filename) as file_in:
+        curr_elf = 1
+        curr_calories = 0
+        for line in file_in:
+            line = line.strip()
+            # new line, new elf
+            if line == '':
+                curr_elf += 1
+                if curr_calories > max_calories:
+                    max_calories = curr_calories
+                    richest_elf = curr_elf
+                # increment current elf and reset calories
+                curr_elf += 1
+                curr_calories = 0
+            else:
+                curr_calories += int(line)
+        # check once more for the last elf, return richest
+        return curr_elf if curr_calories > max_calories else richest_elf
+
+
 if __name__ == '__main__':
     input_file = 'input.txt'
-    print('Part1: {}'.format(day_01(input_file)))
+    print('Part1: {}'.format(day_01_part_1(input_file)))
