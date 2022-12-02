6fb0434c613af3f6ef9dfe51cca3f43e1587bb8b
M. Jahed Hossain <jahedev@gmail.com>
2022-12-02 00:12:27 -0500

    day 1 cpp solution

--- day-01/day01.cpp
+++ day-01/day01.cpp
@@ -1,18 +1,15 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int main() {
-  string filename = "input.txt";
-
-  ifstream ifs(filename);
-
-  long int max_sum;
-  long int sum;
+long int day1_p1(ifstream &ifs) {
+  long int max_sum = -1;
+  long int sum = 0;
 
   string line;
   while (getline(ifs, line)) {
@@ -21,11 +18,49 @@ int main() {
     } else {
       sum += stoi(line);
     }
-    max(sum, max_sum);
+    max_sum = max(sum, max_sum);
   }
-  max_sum = max(sum, max_sum);
 
-  cout << endl << max_sum << endl;
+  return max_sum;
+}
+
+long long int day1_p2(ifstream &ifs) {
+  priority_queue<long int> max_cals;
+
+  long int sum = 0;
+
+  string line;
+  while (getline(ifs, line)) {
+    if (line == "") {
+      max_cals.push(sum);
+      sum = 0;
+    } else {
+      sum += stoi(line);
+    }
+  }
+  max_cals.push(sum);
+
+  long int max_sum = 0;
+
+  for (int i = 1; i <= 3; i++) {
+    max_sum += max_cals.top();
+    max_cals.pop();
+  }
+
+  return max_sum;
+}
+
+int main() {
+  string filename = "input.txt";
+  ifstream ifs(filename);
+
+  long int ans_p1 = day1_p1(ifs);
+  ifs.clear();
+  ifs.seekg(0);
+  long int ans_p2 = day1_p2(ifs);
+
+  cout << "Part 1: " << ans_p1 << endl;
+  cout << "Part 2: " << ans_p2 << endl;
 
   ifs.close();
   return 0;
